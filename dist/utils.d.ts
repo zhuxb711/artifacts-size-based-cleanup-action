b4768eeab552bdb9bd17672cf6a51a94caf79f16
Ruofan <zrfcfgs@outlook.com>
2024-03-27 00:46:57 +0800

    Remove files in dist to make .gitignore works

--- dist/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Utils {
-    static checkPathExists(path: string): Promise<boolean>;
-    static parseMultiLineInputs(input: string): string[];
-    static calcuateMultiPathSize(paths: string[]): Promise<number>;
-    static calculateFolderSize(folderPath: string): Promise<number>;
-}
