76abf6ed4a8dec7f1bcf4e08dd0f0061eaf2053a
Ruofan <zrfcfgs@outlook.com>
2024-03-27 13:39:02 +0800

    Close the zip stream manually to make sure the log print order

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
 import path from 'path';
+import bytes from 'bytes';
+import { promisify } from 'util';
 import _ from 'lodash';
 import * as fs from 'fs';
 import * as fsPromise from 'fs/promises';
@@ -24,49 +26,56 @@ export class Utils {
     const zipStream = fs.createWriteStream(destinationPath, {
       flags: 'ax'
     });
-    const zipArchiver = archiver.create('zip', {
-      zlib: {
-        level: compressionLevel
-      },
-      highWaterMark: zipBuffer
-    });
 
-    zipArchiver.pipe(zipStream);
-    zipArchiver.on('error', (err) => {
-      throw new Error(`An error has occurred during zip creation for '${sourcePath}', message: ${err.message}`);
-    });
-    zipArchiver.on('warning', (err) => {
-      if (err.code === 'ENOENT') {
-        core.warning('ENOENT warning during artifact zip creation. No such file or directory');
-      } else {
-        core.warning(`A non-blocking warning has occurred during artifact zip creation: ${err.code}`);
+    try {
+      const zipArchiver = archiver.create('zip', {
+        zlib: {
+          level: compressionLevel
+        },
+        highWaterMark: zipBuffer
+      });
+
+      zipArchiver.pipe(zipStream);
+      zipArchiver.on('error', (err) => {
+        throw new Error(`An error has occurred during zip creation for '${sourcePath}', message: ${err.message}`);
+      });
+      zipArchiver.on('warning', (err) => {
+        if (err.code === 'ENOENT') {
+          core.warning('ENOENT warning during artifact zip creation. No such file or directory');
+        } else {
+          core.warning(`A non-blocking warning has occurred during artifact zip creation: ${err.code}`);
+        }
+      });
+
+      zipStream.on('open', () => {
+        core.info(`Write stream opened for '${destinationPath}'`);
+      });
+      zipStream.on('error', (err) => {
+        throw new Error(
+          `An error has occurred while creating write stream for '${destinationPath}', message: ${err.message}`
+        );
+      });
+      zipStream.on('close', () => {
+        core.info(`Archiver zipped '${sourcePath}' into ${bytes.format(zipArchiver.pointer())}`);
+      });
+
+      if (await Utils.checkPathExists(sourcePath)) {
+        const fileStats = await fsPromise.stat(sourcePath);
+
+        if (fileStats.isDirectory()) {
+          zipArchiver.directory(sourcePath, false);
+        } else {
+          zipArchiver.file(sourcePath, {
+            name: path.basename(sourcePath)
+          });
+        }
       }
-    });
 
-    zipStream.on('open', () => {
-      console.log(`Write stream opened for '${destinationPath}'`);
-    });
-    zipStream.on('error', (err) => {
-      throw new Error(
-        `An error has occurred while creating write stream for '${destinationPath}', message: ${err.message}`
-      );
-    });
-    zipStream.on('close', () => {
-      console.log(`Archiver zipped ${zipArchiver.pointer()} total bytes`);
-    });
-
-    if (await Utils.checkPathExists(sourcePath)) {
-      const fileStats = await fsPromise.stat(sourcePath);
-
-      if (fileStats.isDirectory()) {
-        zipArchiver.directory(sourcePath, false);
-      } else {
-        zipArchiver.file(sourcePath, {
-          name: path.basename(sourcePath)
-        });
+      await zipArchiver.finalize();
+    } finally {
+      if (!zipStream.closed) {
+        await promisify(zipStream.close)();
       }
     }
-
-    await zipArchiver.finalize();
   }
 }
